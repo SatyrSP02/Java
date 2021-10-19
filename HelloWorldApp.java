2406e20b28881ed424535d209debad423fe94769
Ivan Jandura <jandura.ivan@icloud.com>
2021-10-19 16:36:02 +0200

    added new functionality

--- HelloWorldApp.java
+++ HelloWorldApp.java
@@ -1,5 +1,11 @@
 class HelloWorldApp {
   public static void main(String[] args) {
-    System.out.println("Hello You Asshole");
+
+    String name = "John";
+    String surname = "Malkovich";
+    System.out.println("Hello " + name + " " + surname);
+    System.out.println(name);
+    System.out.println(surname);
+
   }
 }
